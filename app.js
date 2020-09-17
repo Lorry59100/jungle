23f4922425ae630eacc702f6c0cb6bfb3a8ea852
pisha <pisha@hotmail.fr>
2020-09-17 12:31:05 +0200

    animation ScrollMagic img1

--- app.js
+++ app.js
@@ -17,4 +17,17 @@ myAudio.play();
 var controller = new ScrollMagic.Controller();
 
 /* Tween */
-var tween = TweenMax.to("#raptor", 1, {left:800, ease: Power4.easeIn});
+var tween = TweenMax.to("#raptor", 1, {left:800, ease: Power4.easeIn})
+
+/* Scene ScrollMagic */
+var scene = new ScrollMagic.Scene({triggerElement: "#animate1", offset: 400})
+                                  /*  quand le triggerhook va passer à l'endroit du trigger element cela va lancer l'animation */
+                                  .triggerHook(0.4)
+                                  /* Ajout du curseur repère pour trigger */
+                                  .addIndicators({name: "vélociraptor", colorTrigger: "red", indent: 200})
+                                  /* Liaison à Greensock */
+                                  .setTween(tween)
+                                  /* Reverse false pour que l'anim ne se rejoue pas */
+                                  .reverse(false)
+                                  /* Ajout de la scène au controlleur */
+                                  .addTo(controller)
